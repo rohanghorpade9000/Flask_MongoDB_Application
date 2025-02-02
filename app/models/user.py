ca81e621eaefa4d323922c890a869fd4036589bc
rohanghorpade9000 <122144411+rohanghorpade9000@users.noreply.github.com>
2025-02-02 11:31:22 +0530

    Create user.py

---
+++ app/models/user.py
@@ -0,0 +1,11 @@
+
+class User:
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+
+    def to_dict(self):
+        return {
+            "name": self.name,
+            "email": self.email
+        }
